febf48b835c32c787b1059ec093ef3b8d171c57b
Zachary Beese <zachary.beese@gmail.com>
2024-05-01 18:35:42 -0500

    next beacon time added

--- bobcatLogAnalyzer.py
+++ bobcatLogAnalyzer.py
@@ -27,11 +27,23 @@ def parse_line(line):
         match_snr = re.search(pattern_snr, line)
         match_rssi = re.search(pattern_rssi, line)
         match_len = re.search(pattern_len, line)
-        mac = match_dlm.group(1) if match_snr else None
+        mac = match_dlm.group(1) if match_dlm else None
         snr = match_snr.group(1) if match_snr else None
         rssi = match_rssi.group(1) if match_rssi else None
         length = match_len.group(1) if match_len else None
         print("Received Possible Beacon at " + str(date_object) + ": MAC: " + str(mac) + " SNR: " + str(snr) + " rssi: " + str(rssi) + " packet_len: " + str(length))
+    nextBeaconTimeRegex = re.compile(r'next beacon time')
+    matchobject = nextBeaconTimeRegex.search(line)
+    if matchobject:
+        date_string = line.split()[0]
+        date_object = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ")
+        pattern_bt = r'beacon_time=(\d{4}-\d{2}-\d{2} \d{1,2}:\d{2}:\d{2}\.\d+) (\S+)'
+        match_bt = re.search(pattern_bt, line)
+        nextbt_str = match_bt.group(1) if match_bt else None
+        nextbt_offset_str = match_bt.group(2) if match_bt else None
+        dt_with_offset_tz = str(nextbt_str) + " " + str(nextbt_offset_str)
+        print("At " + str(date_object) + ": Next Beacon at: " + str(dt_with_offset_tz))
+
 
 
 def main(argv):
