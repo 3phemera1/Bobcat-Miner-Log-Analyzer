78aa927dfdc2366544a35432fa44d46b745e05d4
Zachary Beese <zachary.beese@gmail.com>
2024-05-01 18:20:10 -0500

    added possible rx of beacon code

--- bobcatLogAnalyzer.py
+++ bobcatLogAnalyzer.py
@@ -14,7 +14,25 @@ def parse_line(line):
         beaconIdRegex = re.compile(r'beacon_id="(.*?)"')
         matchobject = beaconIdRegex.search(line)
         print("Becaon at " + str(date_object) + "   ID is: " + str(matchobject.group(1)))
-#        print(line)
+    beaconRxRegex = re.compile(r'received potential beacon')
+    matchobject = beaconRxRegex.search(line)
+    if matchobject:
+        date_string = line.split()[0]
+        date_object = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ")
+        pattern_dlm = r'downlink_mac=(.*?)'
+        pattern_snr = r'snr: (-?\d+)'
+        pattern_rssi = r'rssi: (-?\d+)'
+        pattern_len = r'len: (\d+)'
+        match_dlm = re.search(pattern_dlm, line)
+        match_snr = re.search(pattern_snr, line)
+        match_rssi = re.search(pattern_rssi, line)
+        match_len = re.search(pattern_len, line)
+        mac = match_dlm.group(1) if match_snr else None
+        snr = match_snr.group(1) if match_snr else None
+        rssi = match_rssi.group(1) if match_rssi else None
+        length = match_len.group(1) if match_len else None
+        print("Received Possible Beacon at " + str(date_object) + ": MAC: " + str(mac) + " SNR: " + str(snr) + " rssi: " + str(rssi) + " packet_len: " + str(length))
+
 
 def main(argv):
     if len(argv) < 2:
