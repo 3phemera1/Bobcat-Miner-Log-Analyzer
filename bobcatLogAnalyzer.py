90e3280bc5823af8693bd121f86cdda557bb002c
Zachary Beese <zachary.beese@gmail.com>
2024-05-01 18:04:39 -0500

    added simple beacon decoding added single argument requirement for IP address of your miner

--- bobcatLogAnalyzer.py
+++ bobcatLogAnalyzer.py
@@ -1,4 +1,33 @@
 #!/usr/bin/python3
+import requests
+import sys
+from requests.auth import HTTPBasicAuth
+import re
+from datetime import datetime
 
-print("Hello there...")
-exit(0)
+def parse_line(line):
+    beaconOutRegex = re.compile(r'poc beacon report')
+    matchobject = beaconOutRegex.search(line)
+    if matchobject:
+        date_string = line.split()[0]
+        date_object = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%fZ")
+        beaconIdRegex = re.compile(r'beacon_id="(.*?)"')
+        matchobject = beaconIdRegex.search(line)
+        print("Becaon at " + str(date_object) + "ID is: " + str(matchobject.group(1)))
+#        print(line)
+
+def main(argv):
+    if len(argv) < 2:
+        print("Usage: ./bobcatLogAnalyzer.py IP_ADDRESS")
+        exit(1)
+    IP = argv[1]
+    url = 'http://' + IP + '/log/console.log'
+    res = requests.get(url, auth=HTTPBasicAuth('bobcat', 'miner'))
+    for line in res.iter_lines():
+#        print(line.decode("ascii"))
+        parse_line(line.decode("ascii"))
+
+
+
+if __name__ == '__main__':
+    main(sys.argv)
